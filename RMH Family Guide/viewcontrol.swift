23c0ee941e2120bc2a607192898545994864a6ec
sameehitamohan <SameeMohan@0587385894.wireless.umich.net>
2017-05-17 14:27:56 -0400

    sigh

---
+++ RMH Family Guide/viewcontrol.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  RMH Family Guide
+//
+//  Created by Sameehita Mohan on 5/17/17.
+//
+//
+
+import UIKit
+
+class viewcontrol: UIViewController {
+    var chosenmap=1;
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+        
+        // You don't need to modify the default init(nibName:bundle:) method.
+        
+    override func loadView() {
+        }
+
+        
+   }
+
+
+
